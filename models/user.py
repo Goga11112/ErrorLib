77b2cef3cdbfe3a4a38026df7af0f69f279baf84
Goga <risk-18@mail.ru>
2025-02-17 13:42:33 +0300

    Увеличил длину символов

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)  # Увеличили длину до 255
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
     def set_password(self, password):
