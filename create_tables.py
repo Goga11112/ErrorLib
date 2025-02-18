1e5d684191ffad0216ab080144cbc831e10090c4
Goga <risk-18@mail.ru>
2025-02-18 11:07:20 +0300

    Добавить админа

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,22 @@
-from app import db, app
+from app import app, db
+from models.user import User
 
 def create_tables():
     with app.app_context():
+        # Создаем таблицы
         db.create_all()
         print("Таблицы успешно созданы.")
         print("Созданы таблицы: Error, ErrorImage")
+        # Создаем пользователя admin
+        admin_user = User.query.filter_by(username='admin').first()
+        if not admin_user:
+            admin_user = User(username='admin')
+            admin_user.set_password('admin')
+            db.session.add(admin_user)
+            db.session.commit()
+            print("Пользователь admin создан")
+        else:
+            print("Пользователь admin уже существует")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     create_tables()
